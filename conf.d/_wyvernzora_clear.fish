65b224661e2edf4f43dbdab503b83b62b7cba616
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2022-01-20 14:12:13 -0800

    Added abbreviation for clear

---
+++ conf.d/_wyvernzora_clear.fish
@@ -0,0 +1,8 @@
+
+function _wyvernzora_clear_install --on-event _wyvernzora_clear_install
+    abbr -aU c clear
+end
+
+function _wyvernzora_clear_uninstall --on-event _wyvernzora_clear_uninstall
+    abbr -eU c
+end
